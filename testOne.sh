6e9aad0259bbee310102c30148a903433aeccea0
Siddharth Singh Rana <91743459+NIXBLACK11@users.noreply.github.com>
2024-01-06 23:17:44 +0530

    Update testOne.sh

--- testOne.sh
+++ testOne.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#bash script to check the output of perl and python script 
+
 perl /home/siddharth/Desktop/programs/mosesdecoder/scripts/tokenizer/normalize-punctuation.perl < /home/siddharth/Desktop/programs/test/test.devtest > pearl_output.txt
 
 python3 /home/siddharth/Desktop/programs/sacremoses/sacremoses/test_normalize.py  /home/siddharth/Desktop/programs/test/test.devtest > python_output.txt
